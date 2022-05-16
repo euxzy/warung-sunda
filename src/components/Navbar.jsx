c2c9a382b7f254503dd79b60d0a454e79e48f318
euxzy <rijal.m12l@gmail.com>
2022-05-16 11:36:42 +0700

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div>Warung Sunda</div>
+      <div>
+        <div>Home</div>
+        <div>Product</div>
+        <div>About</div>
+      </div>
+    </nav>
+  );
+}
