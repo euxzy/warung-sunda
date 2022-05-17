8769129d2d0882e3ce8542c390412f4b9733bf32
euxzy <rijal.m12l@gmail.com>
2022-05-17 08:57:56 +0700

    add home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div>Home</div>
+    </div>
+  );
+}
